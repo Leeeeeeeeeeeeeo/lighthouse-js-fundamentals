556aa72eb892aa1ca820610ba4e272dfae35dcc2
Your Name <akas3524@yahoo.com>
2020-08-20 19:54:07 +0000

    evenNumber function

---
+++ even.js
@@ -0,0 +1,13 @@
+/*const isEven  = function (num) {
+  return num % 2 === 0;
+} 
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}*/
+
+const tenIsEven = isEven(20);
+const elevenIsEven = isEven(4);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
