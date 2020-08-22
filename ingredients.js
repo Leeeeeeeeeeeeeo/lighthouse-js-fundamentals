d8849de0938c1eb6a03ca6e6adf8eb41e26c4197
Your Name <akas3524@yahoo.com>
2020-08-22 14:31:53 +0000

    Literating2

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"]; 
+
+/*for (let i = 0; i < ingredients.length; i++){ 
+  console.log(ingredients[i]);
+} 
+
+let j = 0;
+while(j < ingredients.length){ 
+  console.log(ingredients[j])
+  j++
+} */
+
+for ( let a = ingredients.length - 1; a >= 0; a-- ){
+  console.log(ingredients[a]);
+}
